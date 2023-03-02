37cd4a00370f344243e5ad633de12ee2c547a238
vuh26 <vuh26@up.edu>
2023-03-02 09:28:49 -0800

    Initial commit

---
+++ app/src/main/java/com/example/test2/MainActivity.java
@@ -0,0 +1,18 @@
+package com.example.test2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.GameFramework.LocalGame;
+import com.example.GameFramework.gameConfiguration.GameConfig;
+import com.example.GameFramework.GameMainActivity;
+import com.example.GameFramework.infoMessage.GameState;
+import com.example.test2.R;
+
+
+public class MainActivity extends GameMainActivity {
+
+
+
+}
